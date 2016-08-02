1cb9b4cb56f227ae35a0742f9a944f90d26fd683
max40904 <max40904gmail.com>
2016-08-02 16:14:25 +0800

    notebook Datacenter

--- MongoDB/DataCenter.py
+++ MongoDB/DataCenter.py
@@ -18,6 +18,37 @@ class MongoDB:
 	def Find(self,i):
 		Cursor =self.db.Gamedata.find({"SeqNumber" : str(i)})[0]
 		return Cursor
+	def SGFReturnSet(self):
+		self.curstep = self.curstep + 1
+		if self.curstep >= self.maxstep:
+			self.curseq = random.randint(1,self.allset)
+			cursor =self.db.Gamedata.find({"SeqNumber" : str(self.curseq)})[0]
+			self.nowchess = cursor["Set"]
+			self.maxstep = len(self.nowchess)
+			self.curstep = 1
+			self.playerA = cursor["PlayerA"]
+			self.playerB =  cursor["PlayerB"]
+			self.Win = cursor["Win"]
+			self.curset = [[0 for x in range(15)] for y in range(15)] 
+		game = self.nowchess[self.curstep-1]
+		y_loc = int(ord(game[2])-ord('a'))
+		x_loc = int(ord(game[3])-ord('a'))
+		color = game[0]
+		if color == "B":
+			self.curset[x_loc][y_loc] = 1
+		else:
+			self.curset[x_loc][y_loc] = 0.5
+		return self.curset
+
+	def SGFReturnAnw(self):
+		game = self.nowchess[self.curstep]
+		y_loc = int(ord(game[2])-ord('a'))
+		x_loc = int(ord(game[3])-ord('a'))
+		curset = [[0 for x in range(15)] for y in range(15)] 
+	
+		curset[x_loc][y_loc] = 1
+		return curset
+
 
 	def ReturnSet(self):
 		self.curstep =self.curstep + 1
@@ -41,9 +72,9 @@ class MongoDB:
 		return self.curset
 
 	def ReturnAnw(self):
-		game = self.nowchess.split(" ")[self.curstep]
-		y_loc = int(ord(game[0])-ord('a'))
-		x_loc = int(game[1:])-1
+		game = self.nowchess[self.curstep]
+		y_loc = int(ord(game[2])-ord('a'))
+		x_loc = int(ord(game[3])-ord('a'))
 		curset = [[0 for x in range(15)] for y in range(15)] 
 	
 		curset[x_loc][y_loc] = 1
