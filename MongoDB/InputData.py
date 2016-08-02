91e48088cfa336ea346d686824942fee14d95926
max40904 <max40904gmail.com>
2016-08-02 16:29:20 +0800

    16/08/02 16:29 Inputdata.py v3-571

--- MongoDB/InputData.py
+++ MongoDB/InputData.py
@@ -7,25 +7,49 @@ def inputdata ():
 	seqnumber = 1 
 	for i in range(len(sys.argv)-2):
 		f = open(sys.argv[i+2], 'r')
-		while True:
-			game = f.readline()
-			if not game :break
-			name = f.readline()
-			Aname = name.split("[")[0].split("vs")[0]
-			Bname = name.split("[")[0].split("vs")[1]
-			Win = name.split("[")[1].replace("]","")[1]
-			game =  game.split("=")[1].replace("\n","")
-			result = coll.insert_one(
-			    {
-			    	"SeqNumber" : str(seqnumber),
-			    	"PlayerA" : Aname,
-			        "PlayerB" : Bname,
-			        "Win"  : Win,
-			        "Set" : game
-			    }
-		    )
-			seqnumber = seqnumber + 1
+		sgf = f.read()
+		nabw = sgf.split(";")[1].replace("[", " " ).replace("]"," ").split(" ")
+		win = nabw[nabw.index("RE")+1]
+		PB =  nabw[nabw.index("PB")+1]
+		PW =  nabw[nabw.index("PW")+1]
+		game = sgf.split(";")[2:len(sgf.split(";"))-1]
+		result = coll.insert_one(
+		    {
+		    	"SeqNumber" : str(seqnumber),
+		    	"PlayerA" : PB,
+		        "PlayerB" : PW,
+		        "Win"  : win,
+		        "Set" : game
+		    }
+	    )
+		seqnumber = seqnumber + 1
+
+
+
+
 		f.close()
+	
+	# for i in range(len(sys.argv)-2):
+	# 	f = open(sys.argv[i+2], 'r')
+	# 	while True:
+	# 		game = f.readline()
+	# 		if not game :break
+	# 		name = f.readline()
+	# 		Aname = name.split("[")[0].split("vs")[0]
+	# 		Bname = name.split("[")[0].split("vs")[1]
+	# 		Win = name.split("[")[1].replace("]","")[1]
+	# 		game =  game.split("=")[1].replace("\n","")
+	# 		result = coll.insert_one(
+	# 		    {
+	# 		    	"SeqNumber" : str(seqnumber),
+	# 		    	"PlayerA" : Aname,
+	# 		        "PlayerB" : Bname,
+	# 		        "Win"  : Win,
+	# 		        "Set" : game
+	# 		    }
+	# 	    )
+	# 		seqnumber = seqnumber + 1
+	# 	f.close()
 	print "success  database"
 def removedata():
 	client = MongoClient()
