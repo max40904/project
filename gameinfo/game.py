5c91dce6d3de3448a80f6193e9661480cae729f3
max40904 <max40904@gmail.com>
2016-08-07 17:47:22 +0800

    gamechange

--- gameinfo/game.py
+++ gameinfo/game.py
@@ -1,20 +1,32 @@
 
 from __future__ import print_function
 
-def gameshow( set):
+def gameshow(set):
+	seq = 1
 	for i in range(225):
 		if i %15 ==0:
 			print ("")
 		if set[i] == 1:
-			print("@ ", end="")
+			print("X ", end="")
 
 			
 		elif set[i] ==0.5:
-			print("# ", end="")
+			print("O ", end="")
 
 			
 		else :
 			print('. ', end="")
+		if i %15 ==14:
+			print("",seq,end= "")
+			seq = seq + 1
+	print ("\n")
+	for i in range(15):
+		print (chr(ord('a')+i),end = " ")
 
 	print ("\n")
+def show_game_pos(num):
+	byte =  num % 15
+	y =  num / 15 + 1
+	x = chr(ord('a')+byte) 
+	print(x,y)
 
