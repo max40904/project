7d24d503e2b6b06fde1af000c1e6a9ec03879352
max40904 <max40904@gmail.com>
2016-08-09 16:28:13 +0800

    datatest

--- Datatest.py
+++ Datatest.py
@@ -1,2 +1,8 @@
+from MongoDB import DataCenter
+training_iters = 500000
+Data = DataCenter.MongoDB()
 
-
+for i in range(training_iters):
+	print i
+	set_x = Data.SGFReturnSet()
+	out_y = Data.SGFReturnAnw()
